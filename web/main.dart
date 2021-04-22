927b86db58da521848c85dda940d20206fde7431
JeremyLeland <50390633+JeremyLeland@users.noreply.github.com>
2021-04-22 16:28:33 -0700

    Extract and draw specific tiles
    Also, use a 32x32 version of the cave tiles instead

--- web/main.dart
+++ web/main.dart
@@ -1,15 +1,29 @@
+import 'dart:collection';
 import 'dart:html';
 
 import 'game.dart';
 
 class Arpeagy extends Game {
-
-  final image = new ImageElement(src: 'images/cave.png');
+  final tiles = new HashMap<String, CanvasElement>();
 
   Arpeagy() {
-    image.onLoad.listen((event) {
+    final tileSrc = new ImageElement(src: 'images/cave.png');
+    tileSrc.onLoad.listen((event) {
+
+      tiles['GROUND_1'] = extractTile(tileSrc, 0, 9);
+      tiles['GROUND_2'] = extractTile(tileSrc, 1, 9);
+      tiles['GROUND_3'] = extractTile(tileSrc, 0, 10);
+      tiles['GROUND_4'] = extractTile(tileSrc, 1, 10);
+
       animate();
     });
+  }
+
+  CanvasElement extractTile(ImageElement src, int col, int row, {int WIDTH = 32, int HEIGHT = 32}) {
+    final image = new CanvasElement(width: WIDTH, height: HEIGHT);
+    final ctx = image.context2D;
+    ctx.drawImageScaledFromSource(src, col * WIDTH, row * HEIGHT, WIDTH, HEIGHT, 0, 0, WIDTH, HEIGHT);
+    return image;
   }
 
   @override
@@ -18,7 +32,8 @@ class Arpeagy extends Game {
 
   @override
   void draw(ctx) {
-    ctx.drawImage(image, 0, 0);
+    ctx.drawImage(tiles['GROUND_1']!, 0, 0);
+    ctx.drawImage(tiles['GROUND_2']!, 0, 16);
   }
 }
 
