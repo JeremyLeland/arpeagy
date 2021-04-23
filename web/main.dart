13ea64739d57faa2a9949ad60546fa066dcf4db3
JeremyLeland <50390633+JeremyLeland@users.noreply.github.com>
2021-04-22 21:35:59 -0700

    Moving away from Game object for the moment
    Goal for now is to make an "editor" of sorts to test tiles.
    This will still draw to a canvas, but it will update as changes are made rather than every frame.

--- web/main.dart
+++ web/main.dart
@@ -2,13 +2,13 @@ import 'dart:collection';
 import 'dart:html';
 import 'dart:math';
 
-import 'game.dart';
 
 Map cavesJSON = {
   "src": "images/cave.png",
   "width": 32,
   "height": 32,
   "tiles" : {
+    "empty": {"col": 2, "row": 4},
     "ground1": {"col": 0, "row": 7},
     "ground2": {"col": 1, "row": 7},
     "ground3": {"col": 0, "row": 8},
@@ -20,11 +20,17 @@ Map cavesJSON = {
   }
 };
 
-class Arpeagy extends Game {
+class Arpeagy {
   static const int TILE_WIDTH = 32, TILE_HEIGHT = 32;
+  static const ROWS = 25, COLS = 25;
   final tiles = new HashMap<String, CanvasElement>();
 
+  final tileMap = new List.generate(ROWS, (_) => List.filled(COLS, 'empty', growable: false), growable: false);
+
   Arpeagy() {
+    final canvas = querySelector('#canvas') as CanvasElement;
+    final ctx = canvas.context2D;
+
     final tileSrc = new ImageElement(src: cavesJSON['src']);
     tileSrc.onLoad.listen((event) {
 
@@ -32,28 +38,31 @@ class Arpeagy extends Game {
         tiles[key] = extractTile(tileSrc, value['col'], value['row']);
       });
 
-      drawOnce();
+      final random = new Random();
+      for (var row = 0; row < ROWS; row ++) {
+        for (var col = 0; col < COLS; col ++) {
+          final index = random.nextInt(tiles.keys.length);
+          tileMap[row][col] = tiles.keys.elementAt(index);
+        }
+      }
+
+      draw(ctx);
     });
   }
 
-  CanvasElement extractTile(ImageElement src, int col, int row, {int WIDTH = TILE_WIDTH, int HEIGHT = TILE_HEIGHT}) {
-    final image = new CanvasElement(width: WIDTH, height: HEIGHT);
+  CanvasElement extractTile(ImageElement src, int col, int row) {
+    final w = TILE_WIDTH, h = TILE_HEIGHT;
+    final image = new CanvasElement(width: w, height: h);
     final ctx = image.context2D;
-    ctx.drawImageScaledFromSource(src, col * WIDTH, row * HEIGHT, WIDTH, HEIGHT, 0, 0, WIDTH, HEIGHT);
+    ctx.drawImageScaledFromSource(src, col * w, row * h, w, h, 0, 0, w, h);
     return image;
   }
 
-  @override
-  void update(dt) {
-  }
-
-  @override
-  void draw(ctx) {
-    final random = new Random();
-    for (var x = 0; x < canvasWidth; x += TILE_WIDTH) {
-      for (var y = 0; y < canvasHeight; y += TILE_HEIGHT) {
-        final index = random.nextInt(tiles.values.length);
-        ctx.drawImage(tiles.values.elementAt(index), x, y);
+  void draw(CanvasRenderingContext2D ctx) {
+    for (var row = 0; row < ROWS; row ++) {
+      for (var col = 0; col < COLS; col ++) {
+        final key = tileMap[row][col];
+        ctx.drawImage(tiles[key]!, col * TILE_WIDTH, row * TILE_HEIGHT);
       }
     }
   }
