8b2ccf8ebc35f4e8862cb67e655b7a3889c798b2
JeremyLeland <50390633+JeremyLeland@users.noreply.github.com>
2021-04-23 14:03:53 -0700

    Figure out blend tiles between empty and floor

--- web/main.dart
+++ web/main.dart
@@ -9,16 +9,30 @@ Map cavesJSON = {
   "height": 32,
   "tiles" : {
     "empty": {"col": 2, "row": 4},
-    "ground1": {"col": 0, "row": 7},
-    "ground2": {"col": 1, "row": 7},
-    "ground3": {"col": 0, "row": 8},
-    "ground4": {"col": 1, "row": 8},
-    "ground5": {"col": 0, "row": 9},
-    "ground6": {"col": 1, "row": 9},
-    "ground7": {"col": 0, "row": 10},
-    "ground8": {"col": 1, "row": 10}
+    "empty_floor_SE": {"col" : 6, "row": 5},
+    "empty_floor_S": {"col" : 7, "row": 5},
+    "empty_floor_SW": {"col" : 8, "row": 5},
+    "empty_floor_E": {"col" : 6, "row": 6},
+    "empty_floor_W": {"col" : 8, "row": 6},
+    "empty_floor_NE": {"col" : 6, "row": 7},
+    "empty_floor_N": {"col" : 7, "row": 7},
+    "empty_floor_NW": {"col" : 8, "row": 7},
+    "empty_floor_S+E": {"col" : 6, "row": 8},
+    "empty_floor_S+W": {"col" : 7, "row": 8},
+    "empty_floor_N+E": {"col" : 6, "row": 9},
+    "empty_floor_N+W": {"col" : 7, "row": 9},
+    "floor1": {"col": 0, "row": 7},
+    "floor2": {"col": 1, "row": 7},
+    "floor3": {"col": 0, "row": 8},
+    "floor4": {"col": 1, "row": 8},
+    "floor5": {"col": 0, "row": 9},
+    "floor6": {"col": 1, "row": 9},
+    "floor7": {"col": 0, "row": 10},
+    "floor8": {"col": 1, "row": 10}
   }
 };
+
+enum Cave { empty, floor }
 
 class Arpeagy {
   static const int TILE_WIDTH = 32, TILE_HEIGHT = 32;
@@ -27,7 +41,7 @@ class Arpeagy {
 
   final CanvasElement canvas = querySelector('#canvas') as CanvasElement;
 
-  final tileMap = new List.generate(ROWS, (_) => List.filled(COLS, 'empty', growable: false), growable: false);
+  final typeMap = new List.generate(ROWS, (_) => List.filled(COLS, Cave.empty, growable: false), growable: false);
 
   Arpeagy() {
     canvas.onMouseDown.listen((e) => mouseAction(e));
@@ -40,14 +54,6 @@ class Arpeagy {
         tiles[key] = extractTile(tileSrc, value['col'], value['row']);
       });
 
-      final random = new Random();
-      for (var row = 0; row < ROWS; row ++) {
-        for (var col = 0; col < COLS; col ++) {
-          final index = random.nextInt(tiles.keys.length);
-          tileMap[col][row] = tiles.keys.elementAt(index);
-        }
-      }
-
       draw(canvas.context2D);
     });
   }
@@ -57,7 +63,8 @@ class Arpeagy {
       final col = (event.offset.x / TILE_WIDTH).floor();
       final row = (event.offset.y / TILE_HEIGHT).floor();
 
-      tileMap[col][row] = 'ground1';
+      typeMap[col][row] = Cave.floor;
+
       draw(canvas.context2D);
     }
   }
@@ -70,11 +77,47 @@ class Arpeagy {
     return image;
   }
 
+  CanvasElement getTileAt(List<List<Cave>> typeMap, int col, int row) {
+    final hasLeft = col > 0, hasRight = col < typeMap.length - 1;
+    final hasUp = row > 0, hasDown = row < typeMap[0].length - 1;
+
+    final nw = hasUp && hasLeft    ? typeMap[col-1][row-1] : Cave.empty;
+    final n  = hasUp               ? typeMap[col  ][row-1] : Cave.empty;
+    final ne = hasUp && hasRight   ? typeMap[col+1][row-1] : Cave.empty;
+    final w  = hasLeft             ? typeMap[col-1][row  ] : Cave.empty;
+    final x  = typeMap[col][row];
+    final e  = hasRight            ? typeMap[col+1][row  ] : Cave.empty;
+    final sw = hasDown && hasLeft  ? typeMap[col-1][row+1] : Cave.empty;
+    final s  = hasDown             ? typeMap[col  ][row+1] : Cave.empty;
+    final se = hasDown && hasRight ? typeMap[col+1][row+1] : Cave.empty;
+
+
+    if (x == Cave.floor) {
+      return tiles['floor5']!;
+    }
+    else if (x == Cave.empty) {
+      if (n == Cave.floor && w == Cave.floor)   return tiles['empty_floor_N+W']!;
+      if (n == Cave.floor && e == Cave.floor)   return tiles['empty_floor_N+E']!;
+      if (s == Cave.floor && w == Cave.floor)   return tiles['empty_floor_S+W']!;
+      if (s == Cave.floor && e == Cave.floor)   return tiles['empty_floor_S+E']!;
+      if (n  == Cave.floor)   return tiles['empty_floor_N']!;
+      if (w  == Cave.floor)   return tiles['empty_floor_W']!;
+      if (e  == Cave.floor)   return tiles['empty_floor_E']!;
+      if (s  == Cave.floor)   return tiles['empty_floor_S']!;
+      if (nw == Cave.floor)   return tiles['empty_floor_NW']!;
+      if (ne == Cave.floor)   return tiles['empty_floor_NE']!;
+      if (sw == Cave.floor)   return tiles['empty_floor_SW']!;
+      if (se == Cave.floor)   return tiles['empty_floor_SE']!;
+    }
+
+    return tiles['empty']!;
+  }
+
   void draw(CanvasRenderingContext2D ctx) {
     for (var row = 0; row < ROWS; row ++) {
       for (var col = 0; col < COLS; col ++) {
-        final key = tileMap[col][row];
-        ctx.drawImage(tiles[key]!, col * TILE_WIDTH, row * TILE_HEIGHT);
+        final tile = getTileAt(typeMap, col, row);
+        ctx.drawImage(tile, col * TILE_WIDTH, row * TILE_HEIGHT);
       }
     }
   }
