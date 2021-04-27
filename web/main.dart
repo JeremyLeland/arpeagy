efd885d8de12d3374048f232bf8836c543783081
JeremyLeland <50390633+JeremyLeland@users.noreply.github.com>
2021-04-27 10:16:04 -0700

    Make edge type detection code more general
    Also, making relationships between tiles and their edges explicit in the json

--- web/main.dart
+++ web/main.dart
@@ -1,4 +1,3 @@
-import 'dart:collection';
 import 'dart:html';
 import 'dart:math';
 
@@ -8,71 +7,126 @@ Map cavesJSON = {
   "width": 32,
   "height": 32,
   "tiles" : {
-    "empty": {"col": 2, "row": 4},
-    "empty_floor_SE": {"col" : 6, "row": 5},
-    "empty_floor_S": {"col" : 7, "row": 5},
-    "empty_floor_SW": {"col" : 8, "row": 5},
-    "empty_floor_E": {"col" : 6, "row": 6},
-    "empty_floor_W": {"col" : 8, "row": 6},
-    "empty_floor_NE": {"col" : 6, "row": 7},
-    "empty_floor_N": {"col" : 7, "row": 7},
-    "empty_floor_NW": {"col" : 8, "row": 7},
-    "empty_floor_S+E": {"col" : 6, "row": 8},
-    "empty_floor_S+W": {"col" : 7, "row": 8},
-    "empty_floor_N+E": {"col" : 6, "row": 9},
-    "empty_floor_N+W": {"col" : 7, "row": 9},
-    "water": {"col": 2, "row": 3},
-    "water_floor_SE": {"col" : 0, "row": 0},
-    "water_floor_S": {"col" : 1, "row": 0},
-    "water_floor_SW": {"col" : 2, "row": 0},
-    "water_floor_E": {"col" : 0, "row": 1},
-    "water_floor_W": {"col" : 2, "row": 1},
-    "water_floor_NE": {"col" : 0, "row": 2},
-    "water_floor_N": {"col" : 1, "row": 2},
-    "water_floor_NW": {"col" : 2, "row": 2},
-    "water_floor_S+E": {"col" : 0, "row": 3},
-    "water_floor_S+W": {"col" : 1, "row": 3},
-    "water_floor_N+E": {"col" : 0, "row": 4},
-    "water_floor_N+W": {"col" : 1, "row": 4},
+    "empty": {
+      "col": 2, "row": 4,
+      "edges": {
+        "floor": {
+          "SE": {"col" : 6, "row": 5},
+          "S": {"col" : 7, "row": 5},
+          "SW": {"col" : 8, "row": 5},
+          "E": {"col" : 6, "row": 6},
+          "W": {"col" : 8, "row": 6},
+          "NE": {"col" : 6, "row": 7},
+          "N": {"col" : 7, "row": 7},
+          "NW": {"col" : 8, "row": 7},
+          "S+E": {"col" : 6, "row": 8},
+          "S+W": {"col" : 7, "row": 8},
+          "N+E": {"col" : 6, "row": 9},
+          "N+W": {"col" : 7, "row": 9},
+        }
+      }
+    },
+    "water": {
+      "col": 2, "row": 3,
+      "edges": {
+        "floor": {
+          "SE": {"col" : 0, "row": 0},
+          "S": {"col" : 1, "row": 0},
+          "SW": {"col" : 2, "row": 0},
+          "E": {"col" : 0, "row": 1},
+          "W": {"col" : 2, "row": 1},
+          "NE": {"col" : 0, "row": 2},
+          "N": {"col" : 1, "row": 2},
+          "NW": {"col" : 2, "row": 2},
+          "S+E": {"col" : 0, "row": 3},
+          "S+W": {"col" : 1, "row": 3},
+          "N+E": {"col" : 0, "row": 4},
+          "N+W": {"col" : 1, "row": 4},
+        }
+      }
+    },
     "floor1": {"col": 0, "row": 7},
     "floor2": {"col": 1, "row": 7},
     "floor3": {"col": 0, "row": 8},
     "floor4": {"col": 1, "row": 8},
-    "floor5": {"col": 0, "row": 9},
+    "floor": {"col": 0, "row": 9},
     "floor6": {"col": 1, "row": 9},
     "floor7": {"col": 0, "row": 10},
     "floor8": {"col": 1, "row": 10}
   }
 };
 
-enum Cave { empty, water, floor }
+class EdgeInfo {
+  CanvasElement? northWest, north, northEast, west, east, southWest, south, southEast;
+  CanvasElement? northAndWest, northAndEast, southAndWest, southAndEast;
+}
+
+class TileInfo {
+  late CanvasElement image;
+  final edges = new Map<String, EdgeInfo>();
+
+  TileInfo(Map json, ImageElement src, int width, int height) {
+    image = _extractTile(src, json, width, height);
+
+    if (json.containsKey('edges')) {
+      (json['edges'] as Map).forEach((key, value) {
+        final edgeInfo = new EdgeInfo();
+        edgeInfo.northWest = _extractTile(src, value['NW'], width, height);
+        edgeInfo.north     = _extractTile(src, value['N'],  width, height);
+        edgeInfo.northEast = _extractTile(src, value['NE'], width, height);
+        edgeInfo.west      = _extractTile(src, value['W'],  width, height);
+        edgeInfo.east      = _extractTile(src, value['E'],  width, height);
+        edgeInfo.southWest = _extractTile(src, value['SW'], width, height);
+        edgeInfo.south     = _extractTile(src, value['S'],  width, height);
+        edgeInfo.southEast = _extractTile(src, value['SE'], width, height);
+
+        edgeInfo.northAndWest = _extractTile(src, value['N+W'], width, height);
+        edgeInfo.northAndEast = _extractTile(src, value['N+E'], width, height);
+        edgeInfo.southAndWest = _extractTile(src, value['S+W'], width, height);
+        edgeInfo.southAndEast = _extractTile(src, value['S+E'], width, height);
+
+        edges[key] = edgeInfo;
+      });
+    }
+  }
+
+  CanvasElement _extractTile(ImageElement src, Map json, int width, int height) {
+    final col = json['col'], row = json['row'];
+    final w = width, h = height;
+    final image = new CanvasElement(width: w, height: h);
+    final ctx = image.context2D;
+    ctx.drawImageScaledFromSource(src, col * w, row * h, w, h, 0, 0, w, h);
+    return image;
+  }
+}
+
 
 class Arpeagy {
   static const int TILE_WIDTH = 32, TILE_HEIGHT = 32;
   static const ROWS = 25, COLS = 25;
-  final tiles = new HashMap<String, CanvasElement>();
+  final tiles = new Map<String, TileInfo>();
 
   final canvas = querySelector('#canvas') as CanvasElement;
   final emptyButton = querySelector('#empty') as ButtonElement;
   final waterButton = querySelector('#water') as ButtonElement;
   final floorButton = querySelector('#floor') as ButtonElement;
 
-  final typeMap = new List.generate(ROWS, (_) => List.filled(COLS, Cave.empty, growable: false), growable: false);
-  var clickType = Cave.floor;
+  final typeMap = new List.generate(ROWS, (_) => List.filled(COLS, 'empty', growable: false), growable: false);
+  var clickType = 'floor';
 
   Arpeagy() {
     canvas.onMouseDown.listen((e) => mouseAction(e));
     canvas.onMouseMove.listen((e) => mouseAction(e));
 
-    emptyButton.onClick.listen((_) => clickType = Cave.empty);
-    waterButton.onClick.listen((_) => clickType = Cave.water);
-    floorButton.onClick.listen((_) => clickType = Cave.floor);
+    emptyButton.onClick.listen((_) => clickType = 'empty');
+    waterButton.onClick.listen((_) => clickType = 'water');
+    floorButton.onClick.listen((_) => clickType = 'floor');
 
     final tileSrc = new ImageElement(src: cavesJSON['src']);
     tileSrc.onLoad.listen((event) {
 
       (cavesJSON['tiles'] as Map).forEach((key, value) {
-        tiles[key] = extractTile(tileSrc, value['col'], value['row']);
+        tiles[key] = new TileInfo(value, tileSrc, TILE_WIDTH, TILE_HEIGHT);
       });
 
       draw(canvas.context2D);
@@ -90,64 +144,47 @@ class Arpeagy {
     }
   }
 
-  CanvasElement extractTile(ImageElement src, int col, int row) {
-    final w = TILE_WIDTH, h = TILE_HEIGHT;
-    final image = new CanvasElement(width: w, height: h);
-    final ctx = image.context2D;
-    ctx.drawImageScaledFromSource(src, col * w, row * h, w, h, 0, 0, w, h);
-    return image;
-  }
+  
 
-  CanvasElement getTileAt(List<List<Cave>> typeMap, int col, int row) {
+  CanvasElement getTileAt(List<List<String>> typeMap, int col, int row) {
     final hasLeft = col > 0, hasRight = col < typeMap.length - 1;
     final hasUp = row > 0, hasDown = row < typeMap[0].length - 1;
 
-    final nw = hasUp && hasLeft    ? typeMap[col-1][row-1] : Cave.empty;
-    final n  = hasUp               ? typeMap[col  ][row-1] : Cave.empty;
-    final ne = hasUp && hasRight   ? typeMap[col+1][row-1] : Cave.empty;
-    final w  = hasLeft             ? typeMap[col-1][row  ] : Cave.empty;
+    final nw = hasUp && hasLeft    ? typeMap[col-1][row-1] : '';
+    final n  = hasUp               ? typeMap[col  ][row-1] : '';
+    final ne = hasUp && hasRight   ? typeMap[col+1][row-1] : '';
+    final w  = hasLeft             ? typeMap[col-1][row  ] : '';
     final x  = typeMap[col][row];
-    final e  = hasRight            ? typeMap[col+1][row  ] : Cave.empty;
-    final sw = hasDown && hasLeft  ? typeMap[col-1][row+1] : Cave.empty;
-    final s  = hasDown             ? typeMap[col  ][row+1] : Cave.empty;
-    final se = hasDown && hasRight ? typeMap[col+1][row+1] : Cave.empty;
+    final e  = hasRight            ? typeMap[col+1][row  ] : '';
+    final sw = hasDown && hasLeft  ? typeMap[col-1][row+1] : '';
+    final s  = hasDown             ? typeMap[col  ][row+1] : '';
+    final se = hasDown && hasRight ? typeMap[col+1][row+1] : '';
 
+    final self = tiles[x]!;
+    final adj = {nw, n, ne, w, e, sw, s, se}.where((t) => t != '' && t != x);
 
-    if (x == Cave.floor) {
-      return tiles['floor5']!;
+    if (adj.length == 1) {
+      final other = adj.first;
+
+      if (self.edges.containsKey(other)) {
+        final edge = self.edges[other]!;
+
+        if (n == other && w == other)   return edge.northAndWest ?? self.image;
+        if (n == other && e == other)   return edge.northAndEast ?? self.image;
+        if (s == other && w == other)   return edge.southAndWest ?? self.image;
+        if (s == other && e == other)   return edge.southAndEast ?? self.image;
+        if (n  == other)   return edge.north ?? self.image;
+        if (w  == other)   return edge.west ?? self.image;
+        if (e  == other)   return edge.east ?? self.image;
+        if (s  == other)   return edge.south ?? self.image;
+        if (nw == other)   return edge.northWest ?? self.image;
+        if (ne == other)   return edge.northEast ?? self.image;
+        if (sw == other)   return edge.southWest ?? self.image;
+        if (se == other)   return edge.southEast ?? self.image;
+      }
     }
-    else if (x == Cave.empty) {
-      if (n == Cave.floor && w == Cave.floor)   return tiles['empty_floor_N+W']!;
-      if (n == Cave.floor && e == Cave.floor)   return tiles['empty_floor_N+E']!;
-      if (s == Cave.floor && w == Cave.floor)   return tiles['empty_floor_S+W']!;
-      if (s == Cave.floor && e == Cave.floor)   return tiles['empty_floor_S+E']!;
-      if (n  == Cave.floor)   return tiles['empty_floor_N']!;
-      if (w  == Cave.floor)   return tiles['empty_floor_W']!;
-      if (e  == Cave.floor)   return tiles['empty_floor_E']!;
-      if (s  == Cave.floor)   return tiles['empty_floor_S']!;
-      if (nw == Cave.floor)   return tiles['empty_floor_NW']!;
-      if (ne == Cave.floor)   return tiles['empty_floor_NE']!;
-      if (sw == Cave.floor)   return tiles['empty_floor_SW']!;
-      if (se == Cave.floor)   return tiles['empty_floor_SE']!;
-    }
-    else if (x == Cave.water) {
-      if (n == Cave.floor && w == Cave.floor)   return tiles['water_floor_N+W']!;
-      if (n == Cave.floor && e == Cave.floor)   return tiles['water_floor_N+E']!;
-      if (s == Cave.floor && w == Cave.floor)   return tiles['water_floor_S+W']!;
-      if (s == Cave.floor && e == Cave.floor)   return tiles['water_floor_S+E']!;
-      if (n  == Cave.floor)   return tiles['water_floor_N']!;
-      if (w  == Cave.floor)   return tiles['water_floor_W']!;
-      if (e  == Cave.floor)   return tiles['water_floor_E']!;
-      if (s  == Cave.floor)   return tiles['water_floor_S']!;
-      if (nw == Cave.floor)   return tiles['water_floor_NW']!;
-      if (ne == Cave.floor)   return tiles['water_floor_NE']!;
-      if (sw == Cave.floor)   return tiles['water_floor_SW']!;
-      if (se == Cave.floor)   return tiles['water_floor_SE']!;
-
-      return tiles['water']!;
-    }
-
-    return tiles['empty']!;
+    
+    return self.image;
   }
 
   void draw(CanvasRenderingContext2D ctx) {
