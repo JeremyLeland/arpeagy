c27c56905a159e876235030e3199589218e004c1
JeremyLeland <50390633+JeremyLeland@users.noreply.github.com>
2021-04-27 20:40:32 -0700

    Move json to separate file

--- web/main.dart
+++ web/main.dart
@@ -1,68 +1,7 @@
+import 'dart:convert';
 import 'dart:html';
 
 import 'tiles.dart';
-
-
-Map cavesJSON = {
-  "src": "images/cave.png",
-  "width": 32,
-  "height": 32,
-  "tiles" : {
-    "empty": {
-      "col": 2, "row": 4,
-      "edges": {
-        "floor": {
-          "SE": {"col" : 6, "row": 5},
-          "S": {"col" : 7, "row": 5},
-          "SW": {"col" : 8, "row": 5},
-          "E": {"col" : 6, "row": 6},
-          "W": {"col" : 8, "row": 6},
-          "NE": {"col" : 6, "row": 7},
-          "N": {"col" : 7, "row": 7},
-          "NW": {"col" : 8, "row": 7},
-          "S+E": {"col" : 6, "row": 8},
-          "S+W": {"col" : 7, "row": 8},
-          "N+E": {"col" : 6, "row": 9},
-          "N+W": {"col" : 7, "row": 9},
-        }
-      }
-    },
-    "water": {
-      "col": 2, "row": 3,
-      "edges": {
-        "floor": {
-          "SE": {"col" : 0, "row": 0},
-          "S": {"col" : 1, "row": 0},
-          "SW": {"col" : 2, "row": 0},
-          "E": {"col" : 0, "row": 1},
-          "W": {"col" : 2, "row": 1},
-          "NE": {"col" : 0, "row": 2},
-          "N": {"col" : 1, "row": 2},
-          "NW": {"col" : 2, "row": 2},
-          "S+E": {"col" : 0, "row": 3},
-          "S+W": {"col" : 1, "row": 3},
-          "N+E": {"col" : 0, "row": 4},
-          "N+W": {"col" : 1, "row": 4},
-        }
-      }
-    },
-    "floor": {
-      "col": 0, "row": 9,
-      "variations": [
-        {"col": 0, "row": 9},
-        {"col": 1, "row": 9},
-        {"col": 0, "row": 10},
-        {"col": 1, "row": 10},
-      ],
-      "doodads": [
-        {"col": 0, "row": 7},
-        {"col": 1, "row": 7},
-        {"col": 0, "row": 8},
-        {"col": 1, "row": 8},
-      ]
-    }
-  }
-};
 
 class Arpeagy {
   static const int MAP_COLS = 25, MAP_ROWS = 25;
@@ -89,10 +28,14 @@ class Arpeagy {
       draw(canvas.context2D);
     });
 
-    caveTiles = new TileSet(cavesJSON);
-    caveTiles.ready.then((_) {
-      tileMap = new TileMap(caveTiles, MAP_ROWS, MAP_COLS);
-      draw(canvas.context2D);
+    HttpRequest.getString('json/caveTiles.json').then((jsonString) {
+      Map cavesJson = jsonDecode(jsonString);
+
+      caveTiles = new TileSet(cavesJson);
+      caveTiles.ready.then((_) {
+        tileMap = new TileMap(caveTiles, MAP_ROWS, MAP_COLS);
+        draw(canvas.context2D);
+      });
     });
   }
 
@@ -101,9 +44,10 @@ class Arpeagy {
       final col = (event.offset.x / caveTiles.width).floor();
       final row = (event.offset.y / caveTiles.height).floor();
 
-      tileMap.typeMap[col][row] = clickType;
-
-      draw(canvas.context2D);
+      if (tileMap.typeMap[col][row] != clickType) {
+        tileMap.typeMap[col][row] = clickType;
+        draw(canvas.context2D);
+      }
     }
   }
 
